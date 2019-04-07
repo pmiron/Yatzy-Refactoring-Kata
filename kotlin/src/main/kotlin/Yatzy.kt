d31bb63db1a9557cc8d8f6d065f9aaa5b91a4acb
Pavel Miron <pmiron@outlook.com>
2019-04-07 23:31:17 +0200

    Removed static methods from companion object. Test and class methods renaming

--- kotlin/src/main/kotlin/Yatzy.kt
+++ kotlin/src/main/kotlin/Yatzy.kt
@@ -1,18 +1,69 @@
 
-class Yatzy(d1: Int, d2: Int, d3: Int, d4: Int, _5: Int) {
+class Yatzy(dice1: Int, dice2: Int, dice3: Int, dice4: Int, dice5: Int) {
 
     protected var dice: IntArray = IntArray(5)
 
     init {
-        dice[0] = d1
-        dice[1] = d2
-        dice[2] = d3
-        dice[3] = d4
-        dice[4] = _5
+        dice[0] = dice1
+        dice[1] = dice2
+        dice[2] = dice3
+        dice[3] = dice4
+        dice[4] = dice5
+    }
+
+    fun chance(): Int {
+        var total = 0
+        total += dice[0]
+        total += dice[1]
+        total += dice[2]
+        total += dice[3]
+        total += dice[4]
+        return total
+    }
+
+    fun yatzy(): Int {
+        val counts = IntArray(6)
+        for (die in dice)
+            counts[die - 1]++
+        for (i in 0..5)
+            if (counts[i] == 5)
+                return 50
+        return 0
+    }
+
+    fun ones(): Int {
+        var sum = 0
+        if (dice[0] == 1) sum++
+        if (dice[1] == 1) sum++
+        if (dice[2] == 1) sum++
+        if (dice[3] == 1) sum++
+        if (dice[4] == 1) sum++
+
+        return sum
+    }
+
+    fun twos(): Int {
+        var sum = 0
+        if (dice[0] == 2) sum += 2
+        if (dice[1] == 2) sum += 2
+        if (dice[2] == 2) sum += 2
+        if (dice[3] == 2) sum += 2
+        if (dice[4] == 2) sum += 2
+        return sum
+    }
+
+    fun threes(): Int {
+        var s = 0
+        if (dice[0] == 3) s += 3
+        if (dice[1] == 3) s += 3
+        if (dice[2] == 3) s += 3
+        if (dice[3] == 3) s += 3
+        if (dice[4] == 3) s += 3
+        return s
     }
 
     fun fours(): Int {
-        var sum: Int = 0
+        var sum = 0
         for (at in 0..4) {
             if (dice[at] == 4) {
                 sum += 4
@@ -23,7 +74,7 @@ class Yatzy(d1: Int, d2: Int, d3: Int, d4: Int, _5: Int) {
 
     fun fives(): Int {
         var s = 0
-        var i: Int = 0
+        var i = 0
         while (i < dice.size) {
             if (dice[i] == 5)
                 s = s + 5
@@ -40,195 +91,143 @@ class Yatzy(d1: Int, d2: Int, d3: Int, d4: Int, _5: Int) {
         return sum
     }
 
-    companion object {
 
-        fun chance(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            var total = 0
-            total += d1
-            total += d2
-            total += d3
-            total += d4
-            total += d5
-            return total
+    fun pair(): Int {
+        val counts = IntArray(6)
+        counts[dice[0] - 1]++
+        counts[dice[1] - 1]++
+        counts[dice[2] - 1]++
+        counts[dice[3] - 1]++
+        counts[dice[4] - 1]++
+        var at: Int
+        at = 0
+        while (at != 6) {
+            if (counts[6 - at - 1] >= 2)
+                return (6 - at) * 2
+            at++
         }
-
-        fun yatzy(vararg dice: Int): Int {
-            val counts = IntArray(6)
-            for (die in dice)
-                counts[die - 1]++
-            for (i in 0..5)
-                if (counts[i] == 5)
-                    return 50
-            return 0
-        }
-
-        fun ones(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            var sum = 0
-            if (d1 == 1) sum++
-            if (d2 == 1) sum++
-            if (d3 == 1) sum++
-            if (d4 == 1) sum++
-            if (d5 == 1)
-                sum++
-
-            return sum
-        }
-
-        fun twos(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            var sum = 0
-            if (d1 == 2) sum += 2
-            if (d2 == 2) sum += 2
-            if (d3 == 2) sum += 2
-            if (d4 == 2) sum += 2
-            if (d5 == 2) sum += 2
-            return sum
-        }
-
-        fun threes(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            var s: Int = 0
-            if (d1 == 3) s += 3
-            if (d2 == 3) s += 3
-            if (d3 == 3) s += 3
-            if (d4 == 3) s += 3
-            if (d5 == 3) s += 3
-            return s
-        }
-
-        fun score_pair(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            val counts = IntArray(6)
-            counts[d1 - 1]++
-            counts[d2 - 1]++
-            counts[d3 - 1]++
-            counts[d4 - 1]++
-            counts[d5 - 1]++
-            var at: Int
-            at = 0
-            while (at != 6) {
-                if (counts[6 - at - 1] >= 2)
-                    return (6 - at) * 2
-                at++
-            }
-            return 0
-        }
-
-        fun two_pair(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            val counts = IntArray(6)
-            counts[d1 - 1]++
-            counts[d2 - 1]++
-            counts[d3 - 1]++
-            counts[d4 - 1]++
-            counts[d5 - 1]++
-            var n = 0
-            var score = 0
-            var i = 0
-            while (i < 6) {
-                if (counts[6 - i - 1] >= 2) {
-                    n++
-                    score += 6 - i
-                }
-                i += 1
-            }
-            return if (n == 2)
-                score * 2
-            else
-                0
-        }
-
-        fun four_of_a_kind(_1: Int, _2: Int, d3: Int, d4: Int, d5: Int): Int {
-            val tallies: IntArray = IntArray(6)
-            tallies[_1 - 1]++
-            tallies[_2 - 1]++
-            tallies[d3 - 1]++
-            tallies[d4 - 1]++
-            tallies[d5 - 1]++
-            for (i in 0..5)
-                if (tallies[i] >= 4)
-                    return (i + 1) * 4
-            return 0
-        }
-
-        fun three_of_a_kind(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            val t: IntArray = IntArray(6)
-            t[d1 - 1]++
-            t[d2 - 1]++
-            t[d3 - 1]++
-            t[d4 - 1]++
-            t[d5 - 1]++
-            for (i in 0..5)
-                if (t[i] >= 3)
-                    return (i + 1) * 3
-            return 0
-        }
-
-        fun smallStraight(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            val tallies: IntArray = IntArray(6)
-            tallies[d1 - 1] += 1
-            tallies[d2 - 1] += 1
-            tallies[d3 - 1] += 1
-            tallies[d4 - 1] += 1
-            tallies[d5 - 1] += 1
-            return if (tallies[0] == 1 &&
-                tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1
-            ) 15 else 0
-        }
-
-        fun largeStraight(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            val tallies: IntArray = IntArray(6)
-            tallies[d1 - 1] += 1
-            tallies[d2 - 1] += 1
-            tallies[d3 - 1] += 1
-            tallies[d4 - 1] += 1
-            tallies[d5 - 1] += 1
-            return if (tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1
-                && tallies[5] == 1
-            ) 20 else 0
-        }
-
-        fun fullHouse(d1: Int, d2: Int, d3: Int, d4: Int, d5: Int): Int {
-            val tallies: IntArray
-            var _2 = false
-            var i: Int
-            var _2_at = 0
-            var _3 = false
-            var _3_at = 0
-
-            tallies = IntArray(6)
-            tallies[d1 - 1] += 1
-            tallies[d2 - 1] += 1
-            tallies[d3 - 1] += 1
-            tallies[d4 - 1] += 1
-            tallies[d5 - 1] += 1
-
-            i = 0
-            while (i != 6) {
-                if (tallies[i] == 2) {
-                    _2 = true
-                    _2_at = i + 1
-                }
-                i += 1
-            }
-
-            i = 0
-            while (i != 6) {
-                if (tallies[i] == 3) {
-                    _3 = true
-                    _3_at = i + 1
-                }
-                i += 1
-            }
-
-            return if (_2 && _3)
-                _2_at * 2 + _3_at * 3
-            else
-                0
-        }
+        return 0
     }
+
+    fun twoPair(): Int {
+        val counts = IntArray(6)
+        counts[dice[0] - 1]++
+        counts[dice[1] - 1]++
+        counts[dice[2] - 1]++
+        counts[dice[3] - 1]++
+        counts[dice[4] - 1]++
+        var n = 0
+        var score = 0
+        var i = 0
+        while (i < 6) {
+            if (counts[6 - i - 1] >= 2) {
+                n++
+                score += 6 - i
+            }
+            i += 1
+        }
+        return if (n == 2)
+            score * 2
+        else
+            0
+    }
+
+    fun threeOfAKind(): Int {
+        val t = IntArray(6)
+        t[dice[0] - 1]++
+        t[dice[1] - 1]++
+        t[dice[2] - 1]++
+        t[dice[3] - 1]++
+        t[dice[4] - 1]++
+        for (i in 0..5)
+            if (t[i] >= 3)
+                return (i + 1) * 3
+        return 0
+    }
+
+    fun fourOfAKind(): Int {
+        val tallies = IntArray(6)
+        tallies[dice[0] - 1]++
+        tallies[dice[1] - 1]++
+        tallies[dice[2] - 1]++
+        tallies[dice[3] - 1]++
+        tallies[dice[4] - 1]++
+        for (i in 0..5)
+            if (tallies[i] >= 4)
+                return (i + 1) * 4
+        return 0
+    }
+
+    fun smallStraight(): Int {
+        val tallies = IntArray(6)
+        tallies[dice[0] - 1] += 1
+        tallies[dice[1] - 1] += 1
+        tallies[dice[2] - 1] += 1
+        tallies[dice[3] - 1] += 1
+        tallies[dice[4] - 1] += 1
+        return if (tallies[0] == 1 &&
+            tallies[1] == 1 &&
+            tallies[2] == 1 &&
+            tallies[3] == 1 &&
+            tallies[4] == 1
+        ) 15 else 0
+    }
+
+    fun largeStraight(): Int {
+        val tallies = IntArray(6)
+        tallies[dice[0] - 1] += 1
+        tallies[dice[1] - 1] += 1
+        tallies[dice[2] - 1] += 1
+        tallies[dice[3] - 1] += 1
+        tallies[dice[4] - 1] += 1
+        return if (tallies[1] == 1 &&
+            tallies[2] == 1 &&
+            tallies[3] == 1 &&
+            tallies[4] == 1
+            && tallies[5] == 1
+        ) 20 else 0
+    }
+
+
+    fun fullHouse(): Int {
+        val tallies: IntArray
+        var _2 = false
+        var i: Int
+        var _2_at = 0
+        var _3 = false
+        var _3_at = 0
+
+        tallies = IntArray(6)
+        tallies[dice[0] - 1] += 1
+        tallies[dice[1] - 1] += 1
+        tallies[dice[2] - 1] += 1
+        tallies[dice[3] - 1] += 1
+        tallies[dice[4] - 1] += 1
+
+        i = 0
+        while (i != 6) {
+            if (tallies[i] == 2) {
+                _2 = true
+                _2_at = i + 1
+            }
+            i += 1
+        }
+
+        i = 0
+        while (i != 6) {
+            if (tallies[i] == 3) {
+                _3 = true
+                _3_at = i + 1
+            }
+            i += 1
+        }
+
+        return if (_2 && _3)
+            _2_at * 2 + _3_at * 3
+        else
+            0
+    }
+
 }
 
 
--- kotlin/src/test/kotlin/YatzyTest.kt
+++ kotlin/src/test/kotlin/YatzyTest.kt
@@ -5,107 +5,103 @@ import org.junit.Assert.*
 class YatzyTest {
 
     @Test
-    fun chance_scores_sum_of_all_dice() {
-        val expected = 15
-        val actual = Yatzy.chance(2, 3, 4, 5, 1)
-        assertEquals(expected, actual)
-        assertEquals(16, Yatzy.chance(3, 3, 4, 5, 1))
+    fun chanceScoresSumOfAllDiceTest() {
+        assertEquals(15, Yatzy(2, 3, 4, 5, 1).chance())
+        assertEquals(16, Yatzy(3, 3, 4, 5, 1).chance())
     }
 
     @Test
-    fun yatzy_scores_50() {
-        val expected = 50
-        val actual = Yatzy.yatzy(4, 4, 4, 4, 4)
-        assertEquals(expected, actual)
-        assertEquals(50, Yatzy.yatzy(6, 6, 6, 6, 6))
-        assertEquals(0, Yatzy.yatzy(6, 6, 6, 6, 3))
+    fun yatzyTest() {
+        assertEquals(50, Yatzy(4, 4, 4, 4, 4).yatzy())
+        assertEquals(50, Yatzy(6, 6, 6, 6, 6).yatzy())
+        assertEquals(0, Yatzy(6, 6, 6, 6, 3).yatzy())
     }
 
     @Test
-    fun test_1s() {
-        assertTrue(Yatzy.ones(1, 2, 3, 4, 5) === 1)
-        assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5))
-        assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5))
-        assertEquals(4, Yatzy.ones(1, 2, 1, 1, 1))
+    fun onesTest() {
+        assertEquals(1, Yatzy(1, 2, 3, 4, 5).ones())
+        assertEquals(2, Yatzy(1, 2, 1, 4, 5).ones())
+        assertEquals(0, Yatzy(6, 2, 2, 4, 5).ones())
+        assertEquals(4, Yatzy(1, 2, 1, 1, 1).ones())
     }
 
     @Test
-    fun test_2s() {
-        assertEquals(4, Yatzy.twos(1, 2, 3, 2, 6))
-        assertEquals(10, Yatzy.twos(2, 2, 2, 2, 2))
+    fun twosTest() {
+        assertEquals(4, Yatzy(1, 2, 3, 2, 6).twos())
+        assertEquals(10, Yatzy(2, 2, 2, 2, 2).twos())
     }
 
     @Test
-    fun test_threes() {
-        assertEquals(6, Yatzy.threes(1, 2, 3, 2, 3))
-        assertEquals(12, Yatzy.threes(2, 3, 3, 3, 3))
+    fun threesTest() {
+        assertEquals(6, Yatzy(1, 2, 3, 2, 3).threes())
+        assertEquals(12, Yatzy(2, 3, 3, 3, 3).threes())
     }
 
     @Test
-    fun fours_test() {
+    fun foursTest() {
         assertEquals(12, Yatzy(4, 4, 4, 5, 5).fours())
         assertEquals(8, Yatzy(4, 4, 5, 5, 5).fours())
         assertEquals(4, Yatzy(4, 5, 5, 5, 5).fours())
     }
 
     @Test
-    fun fives() {
+    fun fivesTest() {
         assertEquals(10, Yatzy(4, 4, 4, 5, 5).fives())
         assertEquals(15, Yatzy(4, 4, 5, 5, 5).fives())
         assertEquals(20, Yatzy(4, 5, 5, 5, 5).fives())
     }
 
     @Test
-    fun sixes_test() {
+    fun sixesTest() {
         assertEquals(0, Yatzy(4, 4, 4, 5, 5).sixes())
         assertEquals(6, Yatzy(4, 4, 6, 5, 5).sixes())
         assertEquals(18, Yatzy(6, 5, 6, 6, 5).sixes())
     }
 
     @Test
-    fun one_pair() {
-        assertEquals(6, Yatzy.score_pair(3, 4, 3, 5, 6))
-        assertEquals(10, Yatzy.score_pair(5, 3, 3, 3, 5))
-        assertEquals(12, Yatzy.score_pair(5, 3, 6, 6, 5))
+    fun pairTest() {
+        assertEquals(6, Yatzy(3, 4, 3, 5, 6).pair())
+        assertEquals(10, Yatzy(5, 3, 3, 3, 5).pair())
+        assertEquals(12, Yatzy(5, 3, 6, 6, 5).pair())
     }
 
     @Test
-    fun two_Pair() {
-        assertEquals(16, Yatzy.two_pair(3, 3, 5, 4, 5))
-        assertEquals(16, Yatzy.two_pair(3, 3, 5, 5, 5))
+    fun twoPairTest() {
+        assertEquals(16, Yatzy(3, 3, 5, 4, 5).twoPair())
+        assertEquals(16, Yatzy(3, 3, 5, 5, 5).twoPair())
     }
 
     @Test
-    fun three_of_a_kind() {
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5))
-        assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5))
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5))
+    fun threeOfAKindTest() {
+        assertEquals(9, Yatzy(3, 3, 3, 4, 5).threeOfAKind())
+        assertEquals(15, Yatzy(5, 3, 5, 4, 5).threeOfAKind())
+        assertEquals(9, Yatzy(3, 3, 3, 3, 5).threeOfAKind())
     }
 
     @Test
-    fun four_of_a_knd() {
-        assertEquals(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5))
-        assertEquals(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5))
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 3))
+    fun fourOfAKindTest() {
+        assertEquals(12, Yatzy(3, 3, 3, 3, 5).fourOfAKind())
+        assertEquals(20, Yatzy(5, 5, 5, 4, 5).fourOfAKind())
+        assertEquals(12, Yatzy(3, 3, 3, 3, 3).fourOfAKind())
     }
 
     @Test
-    fun smallStraight() {
-        assertEquals(15, Yatzy.smallStraight(1, 2, 3, 4, 5))
-        assertEquals(15, Yatzy.smallStraight(2, 3, 4, 5, 1))
-        assertEquals(0, Yatzy.smallStraight(1, 2, 2, 4, 5))
+    fun smallStraightTest() {
+        assertEquals(15, Yatzy(1, 2, 3, 4, 5).smallStraight())
+        assertEquals(15, Yatzy(2, 3, 4, 5, 1).smallStraight())
+        assertEquals(0, Yatzy(1, 2, 2, 4, 5).smallStraight())
     }
 
     @Test
-    fun largeStraight() {
-        assertEquals(20, Yatzy.largeStraight(6, 2, 3, 4, 5))
-        assertEquals(20, Yatzy.largeStraight(2, 3, 4, 5, 6))
-        assertEquals(0, Yatzy.largeStraight(1, 2, 2, 4, 5))
+    fun largeStraightTest() {
+        assertEquals(20, Yatzy(6, 2, 3, 4, 5).largeStraight())
+        assertEquals(20, Yatzy(2, 3, 4, 5, 6).largeStraight())
+        assertEquals(0, Yatzy(1, 2, 2, 4, 5).largeStraight())
     }
 
     @Test
-    fun fullHouse() {
-        assertEquals(18, Yatzy.fullHouse(6, 2, 2, 2, 6))
-        assertEquals(0, Yatzy.fullHouse(2, 3, 4, 5, 6))
+    fun fullHouseTest() {
+        assertEquals(18, Yatzy(6, 2, 2, 2, 6).fullHouse())
+        assertEquals(0, Yatzy(2, 3, 4, 5, 6).fullHouse())
     }
 }
